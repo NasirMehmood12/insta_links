139473cb869d5337a56d230b75ca6367ad9b4e18
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-02-19 12:52:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -50,55 +50,90 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-from flask import Flask, jsonify, render_template
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+# from flask import Flask, jsonify, render_template
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
 
-app = Flask(__name__, template_folder="templates")
+# app = Flask(__name__, template_folder="templates")
 
-# Configure headless Chrome for Render
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+# # Configure headless Chrome for Render
+# options = webdriver.ChromeOptions()
+# options.add_argument("--headless")
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# driver = webdriver.Chrome(options=options)
 
-# List of Instagram pages
+# # List of Instagram pages
+# pages = [
+#     {"name": "Kim Kardashian", "url": "https://www.instagram.com/kimkardashian/"},
+#     {"name": "Kylie Jenner", "url": "https://www.instagram.com/kyliejenner/"},
+#     {"name": "Rihanna", "url": "https://www.instagram.com/badgalriri/"},
+#     {"name": "Kanye West", "url": "https://www.instagram.com/ye/"},
+# ]
+
+# # Homepage Route (Fixes 404 Error)
+# @app.route('/')
+# def home():
+#     return render_template("index.html", pages=pages)
+
+# # Function to scrape Instagram links
+# def scrape_instagram_links(page_name):
+#     extracted_links = []
+#     for page in pages:
+#         if page["name"] == page_name:
+#             driver.get(page["url"])
+#             time.sleep(10)
+
+#             links = driver.find_elements(By.TAG_NAME, "a")
+#             for link in links:
+#                 url = link.get_attribute("href")
+#                 if url and ("/p/" in url or "/reel/" in url):
+#                     extracted_links.append(url)
+#             break
+#     return extracted_links
+
+# # API to get links for a specific page
+# @app.route('/get-links/<string:page_name>', methods=['GET'])
+# def get_instagram_links(page_name):
+#     links = scrape_instagram_links(page_name)
+#     return jsonify({"page": page_name, "links": links})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask, jsonify
+from playwright.sync_api import sync_playwright
+
+app = Flask(__name__)
+
+def scrape_instagram_links(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True, args=["--no-sandbox", "--disable-dev-shm-usage"])
+        page = browser.new_page()
+        page.goto(url)
+        page.wait_for_timeout(15000)  # Wait for the page to load completely
+        anchors = page.query_selector_all("a")
+        post_links = [a.get_attribute("href") for a in anchors if a.get_attribute("href") and "/p/" in a.get_attribute("href")]
+        browser.close()
+    return post_links
+
+# Sample pages list
 pages = [
-    {"name": "Kim Kardashian", "url": "https://www.instagram.com/kimkardashian/"},
     {"name": "Kylie Jenner", "url": "https://www.instagram.com/kyliejenner/"},
-    {"name": "Rihanna", "url": "https://www.instagram.com/badgalriri/"},
-    {"name": "Kanye West", "url": "https://www.instagram.com/ye/"},
+    # Add more pages as needed
 ]
 
-# Homepage Route (Fixes 404 Error)
-@app.route('/')
-def home():
-    return render_template("index.html", pages=pages)
-
-# Function to scrape Instagram links
-def scrape_instagram_links(page_name):
-    extracted_links = []
-    for page in pages:
-        if page["name"] == page_name:
-            driver.get(page["url"])
-            time.sleep(10)
-
-            links = driver.find_elements(By.TAG_NAME, "a")
-            for link in links:
-                url = link.get_attribute("href")
-                if url and ("/p/" in url or "/reel/" in url):
-                    extracted_links.append(url)
-            break
-    return extracted_links
-
-# API to get links for a specific page
-@app.route('/get-links/<string:page_name>', methods=['GET'])
+@app.route("/get-links/<string:page_name>", methods=["GET"])
 def get_instagram_links(page_name):
-    links = scrape_instagram_links(page_name)
+    # Find the page from the list
+    page_data = next((page for page in pages if page["name"] == page_name), None)
+    if not page_data:
+        return jsonify({"error": "Page not found"}), 404
+    links = scrape_instagram_links(page_data["url"])
     return jsonify({"page": page_name, "links": links})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
