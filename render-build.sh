4e74bc978a0d568d96098cd2da8950538bcef8e6
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-02-20 10:54:43 +0500

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "Installing Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
