f2ca3aea005a77a73fe2fd64ca415d4f4f0ce380
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-02-24 12:08:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def get_instagram_links():
     try:
         conn = psycopg2.connect(DATABASE_URL)
         cursor = conn.cursor()
-        cursor.execute('SELECT page_name, link, TO_CHAR("timestamp", \'YYYY-MM-DD HH24:MI:SS\') FROM instagram_links')
+        cursor.execute('SELECT page_name, link FROM instagram_links')
         data = cursor.fetchall()
         cursor.close()
         conn.close()
